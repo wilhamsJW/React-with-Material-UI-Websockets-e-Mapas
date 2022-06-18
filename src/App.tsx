6ccd410abdbfe06537f3f3fba05e1d8f1c80df2a
wilhamsJW <wilhams.dev@gmail.com>
2022-06-18 18:46:42 -0300

    chore: create compnent navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { AppBar, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from './theme'
-import DriveEtaIcon from '@mui/icons-material/DriveEta';
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/> 
       {/** CssBaseline Responsável por alterar toda a cor do meu body para dark, onde foi definida em theme, para saber mais consulte a doc do material UI */}
-      {/** AppBar -> menu superior do MAterial UI
-       * position="static" -> para que o menu fique fixo no topo caso a page de scrool
-       * enableColorOnDark -> captura a cor do primary de theme
-       * IconButton -> temos o button padrão mas esse nos ajuda a posicionar o icon 
-       * color="inherit" -> quer dizer que ele vai herdar a cor do component
-       * edge='start' -> sempre alinhado a esquerda
-       */}
-      <AppBar position="static" enableColorOnDark>
-        <Toolbar>
-          <IconButton color="inherit" edge='start'>
-            <DriveEtaIcon/>
-          </IconButton>
-          <Typography variant="h6">Delivery Júnior</Typography>
-        </Toolbar>
-      </AppBar>
+      <Navbar/>
     </ThemeProvider>
   );
 }
