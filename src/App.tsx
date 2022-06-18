810df29113d928ed489273a1284915a5d2877267
wilhamsJW <wilhams.dev@gmail.com>
2022-06-18 19:07:30 -0300

    chore: create select and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, Grid, Select, ThemeProvider, MenuItem, Button } from "@mui/material";
 import theme from './theme'
 import { Navbar } from './components/Navbar'
 
@@ -8,6 +8,26 @@ function App() {
       <CssBaseline/> 
       {/** CssBaseline Responsável por alterar toda a cor do meu body para dark, onde foi definida em theme, para saber mais consulte a doc do material UI */}
       <Navbar/>
+      <Grid container>
+        <Grid item xs={12} sm={3}>
+          <form>
+            {/** fullWidth displayEmpty defaultValue={""} -> faz com que ocupe um maior espaço */}
+            <Select fullWidth displayEmpty defaultValue={""}>
+              <MenuItem value="">
+                <em>Selecione uma rota</em>
+              </MenuItem>
+              <MenuItem value="">
+                Rota 01
+              </MenuItem>
+            </Select>
+            {/** margin: theme.spacing(1) -> irá dá um espaçamento de 8px para todos os lados do meu button, 8px pq o padrão de um px do materail ui é 8px */}
+            <div style={{textAlign: 'center', margin: theme.spacing(1)}}>
+              <Button type='submit' variant="contained">Iniciar Rota</Button>
+            </div>
+          </form>
+        </Grid>
+        <Grid item xs={12} sm={9}></Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
